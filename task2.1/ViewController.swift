d53e2d8591f1f44f2f8ca398e9faf1fe8df86beb
Віктор Семернін <viktor.semernin@uzhnu.edu.ua>
2020-12-01 15:56:48 +0200

    Initial Commit

---
+++ task2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task2.1
+//
+//  Created by Viktor on 01.12.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
